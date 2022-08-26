18b7c78a0e3d230170981d186a7bd6e201bb6311
robin <812121052@qq.com>
2022-08-26 13:41:06 +0800

    修改数据获取位置

--- readconfig.py
+++ readconfig.py
@@ -1,7 +1,7 @@
 import json
 def read_config(param):
     if param == 'db_config':
-        with open('../config/db_config.json','r') as f:
+        with open('db_config.json','r') as f:
             config_param = json.load(f)
         return config_param
 if __name__ == '__main__':
