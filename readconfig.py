6360fd48bb005fce4075aba8ce824fbe0fd38d21
root <root@DESKTOP-HERVRVF.localdomain>
2022-08-27 11:59:21 +0800

    修改_config参数读取

--- readconfig.py
+++ readconfig.py
@@ -4,8 +4,7 @@ def read_config(param):
         with open('db_config.json','r') as f:
             config_param = json.load(f)
         return config_param
-def db_config():
-    return read_config('db_config')
+db_config = read_config('db_config')
 if __name__ == '__main__':
     config_param = read_config('db_config')
     print(config_param)
