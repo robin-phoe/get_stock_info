d4217d9ca19752ffbd4473a556d6c51be7f3302e
robin <812121052@qq.com>
2022-09-12 21:25:04 +0800

    增加基础数据获取

--- get_information.py
+++ get_information.py
@@ -47,7 +47,7 @@ def get_base_info():
     clear_info()
     bk_map  = get_bk_relation()
     s = pub_uti_a.save()
-    for num in range(0,1000):
+    for num in range(0,1):
         num_str = '{:0>3d}'.format(num)
         for capital_num in ['600','601','603','688','002','000','300']:
             stock_id = capital_num + num_str
